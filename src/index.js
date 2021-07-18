c317b7997172df88367fc94b01f55d6a4dc801ea
mralimov <mralimov89@gmail.com>
2021-07-17 22:47:22 -0400

    Wrap root component with Provider

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import ReactDOM from 'react-dom';
-
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
