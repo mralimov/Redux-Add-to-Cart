1a6d2fed49fc9bf923db5af889aa93b5810cb7ef
mralimov <mralimov89@gmail.com>
2021-07-17 23:07:40 -0400

    create uiSlice recuders

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import store from './components/store/reduxStore';
+
 ReactDOM.render(
   <Provider store={store}>
     <App />
