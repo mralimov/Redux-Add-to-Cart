ce8b2297a587fcffb73789527e3c8c9c73ae47b9
Donnie Alimov <mralimov89@gmail.com>
2021-07-24 13:36:15 -0400

    render toggle() action in CartButton

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,18 +1,17 @@
 import classes from './CartButton.module.css';
 import { useDispatch } from 'react-redux';
 import { uiAction } from '../store/ui-slice';
-
+import { useSelector } from 'react-redux';
 const CartButton = (props) => {
-
-const dispatch = useDispatch()
-
+  const dispatch = useDispatch();
+  const cartitem = useSelector((state) => state.cart.totalQuantity);
   const handleCartView = () => {
-dispatch(uiAction.toggle())
-  }
+    dispatch(uiAction.toggle());
+  };
   return (
     <button className={classes.button} onClick={handleCartView}>
       <span>My Cart</span>
-      <span className={classes.badge}>1</span>
+      <span className={classes.badge}>{cartitem}</span>
     </button>
   );
 };
