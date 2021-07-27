ca62a308bfd44643879757480e964632ca8c901b
Donnie Alimov <mralimov89@gmail.com>
2021-07-27 08:56:53 -0400

    crate fetchCartData custom action

--- src/components/store/acrt-actions.js
+++ src/components/store/acrt-actions.js
@@ -1,4 +1,36 @@
 import { uiAction } from './ui-slice';
+import { cartActions } from './cart-slice';
+
+export const fetchCartData = () => {
+  return async (dispatch) => {
+    const fetchData = async () => {
+      const response = await fetch(
+        'https://redux-add-to-cart-default-rtdb.firebaseio.com/cart.json'
+      );
+      if (!response.ok) {
+        throw new Error('Sending cart data failed');
+      }
+
+      const data = await response.json();
+
+      return data;
+    };
+
+    try {
+      const cartData = await fetchData();
+
+      dispatch(cartActions.replaceCartData(cartData));
+    } catch (error) {
+      dispatch(
+        uiAction.showNotification({
+          status: 'error',
+          title: 'Error!!',
+          message: `Fetching cart data failed ${error.message}`,
+        })
+      );
+    }
+  };
+};
 
 export const sendCartData = (cart) => {
   return async (dispatch) => {
