6bf21bf5e1f7dbd1efcad3930452cc51c8dd7b74
Donnie Alimov <mralimov89@gmail.com>
2021-07-27 08:00:29 -0400

    debugging fetch

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
         })
       );
       const response = await fetch(
-        'https://redux-add-to-cart-default-rtdb.firebaseio.com/',
+        'https://redux-add-to-cart-default-rtdb.firebaseio.com/cart.json',
         {
           method: 'PUT',
           body: JSON.stringify(cart),
