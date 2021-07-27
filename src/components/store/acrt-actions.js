cf58154262a625247bcc30aec22bc87a96a86334
Donnie Alimov <mralimov89@gmail.com>
2021-07-27 09:09:52 -0400

    remove conditional boolean from database

--- src/components/store/acrt-actions.js
+++ src/components/store/acrt-actions.js
@@ -46,7 +46,10 @@ export const sendCartData = (cart) => {
         'https://redux-add-to-cart-default-rtdb.firebaseio.com/cart.json',
         {
           method: 'PUT',
-          body: JSON.stringify(cart),
+          body: JSON.stringify({
+            items: cart.items,
+            totalQuantity: cart.totalQuantity,
+          }),
         }
       );
 
