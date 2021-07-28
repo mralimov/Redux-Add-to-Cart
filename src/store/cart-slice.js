afd94e0b4092af34c7c3af38cbe569178c698914
Donnie Alimov <mralimov89@gmail.com>
2021-07-27 21:27:38 -0400

    debug CartItem title.name

---
+++ src/store/cart-slice.js
@@ -0,0 +1,49 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+// const cartInitialState = { items: [], totalQuantity: 0, isChanged: false };
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState: { items: [], totalQuantity: 0, isChanged: false },
+  reducers: {
+    replaceCartData(state, action) {
+      state.totalQuantity = action.payload.totalQuantity;
+      state.items = action.payload.items;
+    },
+
+    addItemToCart(state, action) {
+      const newItem = action.payload;
+      const existingItem = state.items.find((item) => item.id === newItem.id);
+      state.totalQuantity++;
+      state.isChanged = true;
+      if (!existingItem) {
+        state.items.push({
+          id: newItem.id,
+          price: newItem.price,
+          quantity: 1,
+          name: newItem.title,
+          totalPrice: newItem.price,
+        });
+      } else {
+        existingItem.quantity++;
+        existingItem.totalPrice = existingItem.totalPrice + newItem.price;
+      }
+    },
+
+    removeItemFromCart(state, action) {
+      const id = action.payload;
+      const existingItem = state.items.find((item) => item.id === id);
+      state.totalQuantity--;
+      state.isChanged = true;
+      if (existingItem.quantity === 1) {
+        state.items = state.items.filter((item) => item.id !== id);
+      } else {
+        existingItem.quantity--;
+        existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
+      }
+    },
+  },
+});
+
+export const cartActions = cartSlice.actions;
+export default cartSlice;
